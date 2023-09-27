cc09aa2ca440b4021e916a26caa93f1f5d467328
Sabrina Alves <96206813+sabrinalves01@users.noreply.github.com>
2023-09-27 13:31:50 -0300

    Update desafio1.rb

---
+++ desafio1.rb
@@ -0,0 +1,7 @@
+print "Digite se nome: "
+nome = gets.chomp
+print "Qual seu sobrenome?"
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Olá, #{nome} #{sobrenome}. Você possui #{idade} anos!"
